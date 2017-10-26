485bd0f66d81bbd3770c1ea89f1b673419a1f4a3
alcramirez94 <alcramirez94@gmail.com>
2017-10-26 12:50:45 -0500

    Updated MainActivity.

--- app/src/main/java/alcsoft/com/autobalance/MainActivity.java
+++ app/src/main/java/alcsoft/com/autobalance/MainActivity.java
@@ -1,10 +1,10 @@
 package alcsoft.com.autobalance;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,8 +13,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import alcsoft.com.autobalance.internal.PurchaseHandler;
+import alcsoft.com.autobalance.internal.UserVars;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    // Declares Handlers and Objects
+    public static PurchaseHandler purchaseHandler;
+    public static UserVars userVars;
+    public static String AvailSpendingAmt = Float.toString(0.00f);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +30,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,6 +39,31 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        // Call method to initialize handler and object
+        initHandlersandObject();
+    }
+
+    protected void onPause(){
+        super.onPause();
+        // Save all values and pause activity
+        SharedPreferences sharedPreferences = getSharedPreferences("AutoBalanceData", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        // Saves the UserValues to storage
+        editor.putString("UserValues",userVars.getUserVarsForStorage());
+        // Saves the Purchase ArrayList to storage
+        editor.putString("PurchaseList",purchaseHandler.getPurchaseListforStorage());
+        // Saves the Top Value to storage
+        editor.putInt("topValue",purchaseHandler.getTop());
+        // Saves the Total Purchase Amount to storage
+        editor.putFloat("NetPAmt",purchaseHandler.getTotalPAmt());
+        editor.commit();
+    }
+
+    protected void onResume(){
+        super.onResume();
+        // Reload the objects and values from storage and resume activity
+        initHandlersandObject();
     }
 
     @Override
@@ -79,23 +103,43 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        FragmentManager fragmentManager = getSupportFragmentManager();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_main_layout) {
+            // Handle the main page
+            fragmentManager.beginTransaction().replace(R.id.content_frame, new MainFragment()).commit();
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_options_layout) {
+            // Handle Options page
+            fragmentManager.beginTransaction().replace(R.id.content_frame, new SetupFragment()).commit();
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        } else if (id == R.id.nav_purchases_layout){
+            // Handle Transactions page
+            fragmentManager.beginTransaction().replace(R.id.content_frame, new PurchasesFragment()).commit();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    // Method to initialize handlers and objects
+    private void initHandlersandObject(){
+        // Loads the sharedPreferences
+        SharedPreferences sharedPreferences = getSharedPreferences("AutoBalanceData", Context.MODE_PRIVATE);
+        // Loads the UserVar object in a temp string and loads it to the object
+        String data = sharedPreferences.getString("UserValues","none");
+        userVars = new UserVars(data);
+
+        // Loads the PurchaseList object in a temp string and loads it to the handler object
+        data = sharedPreferences.getString("PurchaseList","none");
+        int tempint = sharedPreferences.getInt("TopValue",0);
+        float tempfl = sharedPreferences.getFloat("NetPAmt",0.00f);
+        purchaseHandler = new PurchaseHandler(data,tempint,tempfl);
+    }
+
+    public static void updateSpendingAmt(){
+        AvailSpendingAmt = String.format("%.02f",userVars.getUserNetIncome() - purchaseHandler.getTotalPAmt());
+    }
+
 }
