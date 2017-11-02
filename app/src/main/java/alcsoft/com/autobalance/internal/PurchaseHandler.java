a1528b5e06e282837f0c0f076ff2d8ff49d21f3a
alcramirez94 <alcramirez94@gmail.com>
2017-11-02 14:23:19 -0500

    Optimized PurchaseHandler Object. Updated class to 2.0 standard. Introduced add,edit,delete methods.

--- app/src/main/java/alcsoft/com/autobalance/internal/PurchaseHandler.java
+++ app/src/main/java/alcsoft/com/autobalance/internal/PurchaseHandler.java
@@ -7,52 +7,52 @@ import java.util.ArrayList;
 
 /**
  * PurchaseHandler Object
- * This object handles the ArrayList of Purchase objects. Provides accessors and mutators functions
- * to the ArrayList. It also returns the ArrayList as a JSON object for storage use.
+ * This object handles the ArrayList of Purchase objects and provides methods to access and mutate it.
+ * It also returns the arraylist as a reference or a formatted JSON object for storage.
  *
- * Created by ALCRamirez94 on 8/16/2017. Revised 10/27/2017
- * Ver 1.1
+ * Created by ALCRamirez94 on 11/1/2017.
+ * Ver 2.0
  */
 
 public class PurchaseHandler {
     private ArrayList<Purchase> PurchaseList;
-    private float TotalPAmt;
+    private float TotalPurchaseAmt;
     private int Top;
 
-    // Default Constructor
-    public PurchaseHandler(String inlist, int intop, float inpamt){
+    // Constructor
+    public PurchaseHandler(String purchaseList, int top, float amt){
         // Checks if list exists
-        if(inlist.equals("none")){
-            // Initialize default values
-            PurchaseList = new ArrayList<Purchase>();
-            TotalPAmt = 0.00f;
+        if(purchaseList.equals("none")){
+            // Creates an Empty list and uses Default values.
+            PurchaseList = new ArrayList<>();
+            TotalPurchaseAmt = 0.00f;
             Top = 0;
         }else{
             // Loads the already existing list from storage
-            // Reads the ArrayList from JSON to ArrayList
+            // Loads the GSON module to read gson strings.
             Gson gson = new Gson();
-            String json = inlist;
+            // Defines the type of object to look for
             Type type = new TypeToken<ArrayList<Purchase>>() {}.getType();
-            // Loads the Transaction List
-            PurchaseList = gson.fromJson(json,type);
+            // Loads the Transaction List from the json string
+            PurchaseList = gson.fromJson(purchaseList,type);
             // Loads the Top Value
-            Top = intop;
+            Top = top;
             // Loads the Total Purchase Amount
-            TotalPAmt = inpamt;
+            TotalPurchaseAmt = amt;
         }
     }
 
-    // ArrayList Mutators
+    // Mutators
     public void addPurchase(String namein, String datein, float amtin){
         // Adds the top value by 1
         Top = Top + 1;
         // Overflow check
-        if(TotalPAmt+amtin >= 99999999.99f){
+        if(TotalPurchaseAmt+amtin >= 99999999.99f){
             // Set to maximum value.
-            TotalPAmt = 9999999.99f;
+            TotalPurchaseAmt = 9999999.99f;
         }else {
             // Adds the total float value
-            TotalPAmt = TotalPAmt + amtin;
+            TotalPurchaseAmt = TotalPurchaseAmt + amtin;
         }
 
         // Creates a new purchase object
@@ -61,50 +61,78 @@ public class PurchaseHandler {
         PurchaseList.add(0,purchase);
     }
 
-    // Reset entire list
-    public void resetList(){
+    public void editPurchaseAt(int position, String name, float amt, String date){
+        // Edits purchase at position, only values that are not empty gets edited.
+        // Gets the purchase object from the list at position
+        Purchase purchase = PurchaseList.get(position);
+
+        // Checks if name is empty
+        // Updates the name on the object
+        if (!name.isEmpty()) purchase.setPurchaseName(name);
+
+        // Checks if float is -1
+        // Updates the amount on the object
+        if (amt != -1.00f) purchase.setPurchaseAmt(amt);
+
+        // Checks if date is empty
+        // Updates the date on the object
+        if (!date.isEmpty()) purchase.setPurchaseDate(date);
+
+        // Replaces the object at the position with the updated information.
+        PurchaseList.set(position,purchase);
+    }
+
+    public void removePurchaseAt(int position){
+        // Removes one item off the list at position.
+        // Updates the top value
+        Top = Top - 1;
+        // Pulls the object from PurchaseList at position into a temp object
+        Purchase purchase = PurchaseList.get(position);
+        // Updates the TotalPurchaseAmount
+        TotalPurchaseAmt = TotalPurchaseAmt - purchase.getPurchaseAmt();
+        // Removes it from the list.
+        PurchaseList.remove(position);
+    }
+
+    public void removeAllPurchases(){
+        // Removes all purchases from list and resets values to default.
         PurchaseList.clear();
         Top = 0;
-        TotalPAmt = 0.00f;
+        TotalPurchaseAmt = 0.00f;
     }
 
     public void removeLastTransaction(){
         // Underflow Check
-        if(Top == 0){
-            // Underflow, Do Nothing
-        }else{
+        if (Top != 0) {
             // Subtract the Top value by 1
             Top = Top - 1;
             // Pulls the Object into a temp object
             Purchase purchase = PurchaseList.get(0);
             // Subtracts the purchase amt from the total
-            TotalPAmt = TotalPAmt - purchase.getPAmt();
+            TotalPurchaseAmt = TotalPurchaseAmt - purchase.getPurchaseAmt();
             // Removes the purchase object from the ArrayList
             PurchaseList.remove(0);
         }
     }
 
     // ArrayList Accessors
-    public String getPurchaseListforStorage(){
+    public String getDataToSave(){
         Gson gson = new Gson();
-        String json = gson.toJson(PurchaseList);
-        return json;
+        return gson.toJson(PurchaseList);
     }
 
-    public ArrayList<Purchase> getPurchaseListReference(){
+    public ArrayList<Purchase> getPurchaseList(){
         return PurchaseList;
     }
 
-    // TotalPAmt Accessors
-    public float getTotalPAmt(){
-        return TotalPAmt;
+    public float getTotalPurchaseAmt(){
+        return TotalPurchaseAmt;
     }
 
     public String getTotalPAmtString(){
-        return String.format(java.util.Locale.US,"%.2f",TotalPAmt);
+        return String.format(java.util.Locale.US,"%.2f",TotalPurchaseAmt);
     }
 
-    // Top Accessor
     public int getTop(){
         return Top;
     }
